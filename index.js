2756b65007f38474053ca6ff33f132ac42e9a8bd
Alamgir4597 <rrony45@gmail.com>
2022-05-27 02:26:41 +0600

    order

--- index.js
+++ index.js
@@ -3,8 +3,85 @@ const cors = require('cors')
 const app= express();
 const port=process.env.PORT  || 5000;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 
 app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq3pw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifJWT(req,res,next) {
+    const authHeader=req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized Access'})
+    }
+    const token=authHeader.split('')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err,decoded){
+        if(err){
+            return res.status(403).send({message: 'fobidden'})
+        }
+        req.decoded=decoded;
+        next();
+    })
+}
+
+async function run() {
+    try {
+        await client.connect();
+        console.log(' db connected');
+        // const productCollection = client.db('assignment-eleven-db').collection('products');
+        const userCollectoin = client.db('assignment-12-db').collection('user');
+        const partsCollectoin = client.db('assignment-12-db').collection('parts');
+        const orderCollectoin = client.db('assignment-12-db').collection('order');
+
+        app.put('/user/:email', async(req,res)=>{
+            const email=req.params.email;
+            const user=req.body;
+            const filter={email:email};
+            const options={upsert:true};
+            const updateDoc={
+                $set:user
+            };
+            const result=await userCollectoin.updateOne(filter,updateDoc,options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN ,{expiresIn:'1h'} );
+            res.send({result,token})
+        });
+
+        app.get('/user', async (req,res)=>{
+            const users= await userCollectoin.find().toArray();
+            res.send(users)
+        });
+
+        app.get('/patrs', async (req,res)=>{
+            const parts = await partsCollectoin.find().toArray();
+            res.send(parts)
+        });
+
+
+        app.post('/addpart', async (req, res) => {
+            newParts = req.body;
+            console.log('add new product', newParts);
+            const result = await partsCollectoin.insertOne(newParts);
+            res.send(result);
+        });
+         app.post('/order', async (req, res) => {
+            newOrder = req.body;
+             console.log('add new product', newOrder);
+            const result = await orderCollectoin.insertOne(newOrder);
+            res.send(result);
+        });
+
+        
+    } finally {
+        // Ensures that the client will close when you finish/error
+        
+    }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('i am backend')
